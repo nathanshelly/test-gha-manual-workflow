aba995656e3915b574aab62123306f037e194b01
Nathan Shelly <nathan.shelly@opendoor.com>
2022-08-10 17:59:24 -0400

    wip

--- .github/scripts/js_auto_update.sh
+++ .github/scripts/js_auto_update.sh
@@ -11,4 +11,4 @@ curl -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: token $AUTOCOMMIT_TOKEN" \
   https://api.github.com/repos/nathanshelly/test-gha-manual-workflow/actions/workflows/js-fix-merge-conflicts.yaml/dispatches \
-  -d '{"ref":"foo","inputs":{"branch":"foo", "name":"Mona the Octocat"}}'
+  -d '{"ref":"main","inputs":{"branch":"foo", "name":"Mona the Octocat"}}'
