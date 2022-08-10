bce07974f8c41cb39eac6879a3e9281655fc5c1b
Nathan Shelly <nathan.shelly@opendoor.com>
2022-08-10 18:01:47 -0400

    wip

--- .github/scripts/js_merge.sh
+++ .github/scripts/js_merge.sh
@@ -9,11 +9,13 @@ echo "Branch: $GITHUB_BRANCH"
 
 echo "test $GITHUB_BRANCH | $GITHUB_HEAD_REF" >> bar
 
+cat bar
+
 git config --global user.email "nsnshelly@gmail.com"
 git config --global user.name "Nathan Shelly"
 
 git add .
 git commit -m 'test change'
 
-git push --quiet https://"${AUTOCOMMIT_TOKEN}"@github.com/opendoor-labs/code.git \
+git push --quiet https://"${AUTOCOMMIT_TOKEN}"@github.com/nathanshelly/test-gha-manual-workflow.git \
   "$GITHUB_HEAD_REF"
